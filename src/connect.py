d85c54663f5da50ff470a4fe60bd4cd08cd91030
Nick Wylynko <nick1014375@gmail.com>
2020-03-15 20:28:56 +0800

    change to connect to node server

--- src/connect.py
+++ src/connect.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 # pip
-from aiohttp import web
 import socketio
 
 # global
@@ -13,24 +12,16 @@ import parser
 import utils
 import handler
 
-# creates a Socket and http (thou dont need the http server) Server
-sio = socketio.AsyncServer()
-app = web.Application()
-sio.attach(app)
-
+# creates a Socket Client
+sio = socketio.Client()
+sio.connect('http://localhost:3001')
 
 @sio.on('map_publish')
-async def map_message(sid, message):
+def map_message(message):
     data = parser.Base64MessageToDictionary(message)
-    
-    print(data)
 
     # utils.logToFile(data)
 
     # handler.Frame(data)
 
     # handler.LandMarks(data)
-
-
-# run on port 3000 as the openvslam connects to a socket server on this port
-web.run_app(app, port=3000)
